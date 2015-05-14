3433ad91bd3bbd37b6d614d5684798ed3db5db08
Guest User <student@epicodus.com>
2015-05-14 14:21:13 -0700

    It adds validations and workind add category id

--- app.rb
+++ app.rb
@@ -13,13 +13,30 @@ end
 
 
 get'/add_recipe' do
-  erb :add_recipes
+  @categories = Category.all
+erb :add_recipes
 end
 
 post '/add_recipe' do
+  
   name = params.fetch('name')
   instructions = params.fetch('instructions')
   rating = params.fetch('rating')
   new_recipe = Recipe.create({name: name, instruction: instructions, rating: rating})
-  redirect '/'
+  category_id = params.fetch("category_id")
+  category = Category.find(category_id)
+  new_recipe.categories.push(category)
+  redirect '/categories'
+end
+
+get '/categories' do
+  @categories = Category.all
+  erb :categories
+end
+
+
+post "/add_cuisine" do
+  cuisine = params.fetch("name")
+  Cuisine.create({cuisine: cuisine})
+  redirect '/categories'
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,3 +4,18 @@ require 'bundler/setup'
 require 'shoulda/matchers'
 Bundler.require(:default, :test)
 Dir[File.dirname(__FILE__) + '/../lib/*.rb'].each {|file| require file}
+
+
+RSpec.configure do |config|
+  config.after(:each) do
+    Category.all().each() do |category|
+      category.destroy()
+    end
+    Recipe.all().each() do |recipe|
+      recipe.destroy()
+    end
+    Ingredient.all().each() do |ingredient|
+      ingredient.destroy()
+    end
+  end
+end
